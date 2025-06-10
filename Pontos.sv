e01e2ba70a03caa34dfe332fdd7ed4a1e1c1b62e
GncRodrigo <rmgoncalves06@gmail.com>
2025-06-10 16:08:15 -0300

    creating the module

---
+++ Pontos.sv
@@ -0,0 +1,11 @@
+module PontosFlutuantes(
+    input logic clock ,
+    input logic reset,
+    input logic [0:31] op_A_in,
+    input logic [0:31] op_B_in,
+    output logic [0:31] data_out,
+    output logic [0:3] status_out,
+    output logic [0:
+
+);
+endmodule
