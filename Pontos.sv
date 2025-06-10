851f463e7ae88458014cf521accfa0b48ed74ec3
GncRodrigo <rmgoncalves06@gmail.com>
2025-06-10 20:24:01 -0300

    completei o modulo

--- Pontos.sv
+++ Pontos.sv
@@ -5,7 +5,11 @@ module PontosFlutuantes(
     input logic [0:31] op_B_in,
     output logic [0:31] data_out,
     output logic [0:3] status_out,
-    output logic [0:
-
+    output logic [0:31] flags_out
 );
 endmodule
+
+typedef enum logic [3:0] { 
+    
+
+} state_t;
